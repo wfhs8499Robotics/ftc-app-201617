7cef4ca88e678db92ac38a2e5b31d75a7547790a
mohair19 <roshani dubel@ad.husd.org>
2016-10-20 16:11:58 -0700

    lots of changes to Chris Auto
    fun!!  It worked

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chrisauto.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chrisauto.java
@@ -1,6 +1,7 @@
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name = "Ftc Test autonomous", group = "Autonomous OpMode")
 //@Override
@@ -10,15 +11,37 @@ public class chrisauto extends LinearOpMode {
         DcMotor leftmotor = null;
         DcMotor rightmotor = null;
         leftmotor = hardwareMap.dcMotor.get("left motor");
+        leftmotor.setDirection(DcMotor.Direction.REVERSE);
         rightmotor = hardwareMap.dcMotor.get("right motor");
 
-        waitForStart();
+        telemetry.addData(">", "Finished initiating, press play to start moving");
+        telemetry.update();
 
+        waitForStart();
+        telemetry.addData(">", "Started");
+        telemetry.update();
         leftmotor.setPower(1.0d);
         rightmotor.setPower(1.0d);
         sleep(2000);
+        telemetry.addData(">", "right");
+        telemetry.update();
+        leftmotor.setPower(1.0d);
+        rightmotor.setPower(0);
+        sleep(1000);
+        telemetry.addData(">", "short");
+        telemetry.update();
+        leftmotor.setPower(1.0d);
+        rightmotor.setPower(1.0d);
+        sleep(1000);
+        telemetry.addData(">", "left");
+        telemetry.update();
+        leftmotor.setPower(0);
+        rightmotor.setPower(1.0d);
+        sleep(1000);
         leftmotor.setPower(0);
         rightmotor.setPower(0);
+        telemetry.addData(">", "Ended");
+        telemetry.update();
 
     }
 }
